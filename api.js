59850a3fd3f5b6e76077381778595f792fd96f65
Michael Romay <amerror@gmail.com>
2020-01-27 16:58:15 -0800

    added database

---
+++ api.js
@@ -0,0 +1,123 @@
+export const productData = [
+    {
+        id: 'r2-bag',
+        name: 'R2D2 Travel Bag',
+        image: '../assets/bag.jpg',
+        description: 'Dumb bag with some Disney stuff on it, IDK'
+    },
+    {
+        id: 'banana',
+        name: 'Banana Slicer',
+        image: '../assets/banana.jpg',
+        description: 'Thing that slices bananas'
+    },
+    {
+
+        id: 'bathroom-pad',
+        name: 'Bathroom iPad Holder',
+        image: '../assets/bathroom.jpg',
+        description: 'This might be gross, but someone is bound to want it'
+    },
+    {
+        id: 'boots',
+        name: 'Dumb Rainboots',
+        image: '../assets/boots.jpg',
+        description: 'Dumb rainboots that keep your feet wet!'
+    },
+    {
+        id: 'breakfast',
+        name: 'Useless Breakfast Thing',
+        image: '../assets/breakfast.jpg',
+        description: 'Make some inconvenient breakfast!'
+    },
+    {
+        id: 'meatball-gum',
+        name: 'Meatball Gum',
+        image: '../assets/bubblegum.jpg',
+        description: 'Well, this is gross.  If you have ever wanted to chew gum that tasted like meat, this product is for you!'
+    },
+    {
+        id: 'chair',
+        name: 'Very Uncomfortable Chair',
+        image: '../assets/chair.jpg',
+        description: 'Dumb chair that is hard to sit in.'
+    },
+    {
+        id: 'cthulu',
+        name: 'Cthulhu',
+        image: '../assets/cthulhu.jpg',
+        description: 'Cthulhu might look cool and be the destroyer of worlds, but unfortunately was created by a well known racist'
+    },
+    {
+        id: 'dog-duck',
+        name: 'Dog Duck Bill',
+        image: '../assets/dog-duck.jpg',
+        description: 'Duck Bill for yer Doggie!'
+    },
+    {
+        id: 'dragon',
+        name: 'Dragon Meat',
+        image: '../assets/dragon.jpg',
+        description: 'Eat some Dragon Meat!'
+    },
+    {
+        id: 'pen',
+        name: 'Cutlery Pens',
+        image: '../assets/pen.jpg',
+        description: 'Ever wanted to eat and write, not nessisarly at the same time, but shortly before or after?  This product is for you!'
+    },
+    {
+        id: 'pet-sweep',
+        name: 'Pet Sweep',
+        image: '../assets/pet-sweep.jpg',
+        description: 'Make your lazy pet do some dang housework for once!'
+    },
+    {
+        id: 'pizza-scissors',
+        name: 'Pizza Scissors',
+        image: '../assets/scissors.jpg',
+        description: 'Cut yer pizza and then put it on a plate'
+    },
+    {
+        id: 'shark',
+        name: 'Shark Sleeping Bag',
+        image: '../assets/shark.jpg',
+        description: 'Sleep inside of a shark'
+    },
+    {
+        id: 'sweep-baby',
+        name: 'Sweep Baby',
+        image: '../assets/sweep.png',
+        description: 'Make your lazy child do some dang housework!'
+    },
+    {
+        id: 'tauntaun',
+        name: 'Taun Taun Sleeping Bag',
+        image: '../assets/tauntaun.jpg',
+        description: 'Disney Sleeping Bag'
+    },
+    {
+        id: 'unicorn',
+        name: 'Unicorn Meat',
+        image: '../assets/unicorn.jpg',
+        description: 'Eat some Unicorn Meat!'
+    },
+    {
+        id: 'usb',
+        name: 'USB Tentacle Drive',
+        image: '../assets/usb.gif',
+        description: 'It is a USB Flash drive, but way more annoying'
+    },
+    {
+        id: 'water-can',
+        name: 'Useless Water Can',
+        image: '../assets/water-can.jpg',
+        description: 'A dumb way to water your plants'
+    },
+    {
+        id: 'wine-glass',
+        name: 'Dumb Wine Glass',
+        image: '../assets/wine-glass.jpg',
+        description: 'A really useless wine glass so you can easily spill on yourself!'
+    },
+];
